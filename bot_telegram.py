10b66c1cf0d27e34d835c089ce9f4c5781702a1c
caio reis <gameplayers176@gmail.com>
2024-05-02 00:04:25 -0300

    testelinhas

--- bot_telegram.py
+++ bot_telegram.py
@@ -20,7 +20,7 @@ def callback_handler(call):
 
         # Enviar a mensagem com a imagem e o botão de pagamento
         with open('./imagem/vip.png', 'rb') as photo:
-            bot.send_photo(call.message.chat.id, photo, caption="*♦️VIDEOS TODOS OS DIAS♦️*\n\n+DE 99VIDEOS\nestou esperando você bem molhadinha no meu VIP😍\n\n💠CLIQUE PRA VIRAR VIP💠", parse_mode="Markdown", reply_markup=layout_pagamento)
+            bot.send_photo(call.message.chat.id, photo, caption="*♦️VIDEOS TODOS OS DIAS♦️*\n\n+DE 99VIDEOS\nSEXO\nMASTURBAÇÃO\nANAL\nSEXO A 3\n\nEstou esperando você bem molhadinha no meu VIP😍\n\n💠CLIQUE PRA VIRAR VIP💠", parse_mode="Markdown", reply_markup=layout_pagamento)
 
 @bot.message_handler(func=lambda message: True)
 def mensagem_genérica(mensagem):
