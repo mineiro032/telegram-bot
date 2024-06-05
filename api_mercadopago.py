351497b5e568d20436a73c47d61a37dc1c4a2339
caio reis <gameplayers176@gmail.com>
2024-06-05 16:27:47 -0300

    teste pix

--- api_mercadopago.py
+++ api_mercadopago.py
@@ -1,19 +1,16 @@
 import mercadopago
 
-
-def gerar_link_de_pagamento():
+def gerar_pix_pagamento():
     sdk = mercadopago.SDK("APP_USR-520820846840078-043012-94bb70878cdd1c3a495338af368ccaeb-336930514")
 
     payment_data = {
-        "items": [
-            {
-                "id": "520820846840078",
-                "title": "VIP",
-                "quantity": 1,
-                "currency_id": "BRL",
-                "unit_price": 9.99,
-            },
-        ],
+        "transaction_amount": 9.99,
+        "description": "Assinatura VIP",
+        "payment_method_id": "pix",
+        "payer": {
+            "email": "user@example.com"
+        }
+    }
         "back_urls": {
             "success": "https://t.me/+33V5tRO6NwU1ZGUx",
             "failure": "http://test.com/failure/compraerrada",
--- bot_telegram.py
+++ bot_telegram.py
@@ -8,20 +8,19 @@ bot = telebot.TeleBot(CHAVE_API)
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     if call.data == "mensagem_nova":
-        # Gerar o link de pagamento
-        link_pagamento = gerar_link_de_pagamento()
+        # Gerar o QR Code do PIX
+pix_code = gerar_pix_pagamento()
 
-        # Criar botão de pagamento
-        botao_pagamento = types.InlineKeyboardButton("ASSINAR VIP R$9,99", url=link_pagamento)
+# Criar o botão de pagamento via PIX
+botao_pagamento = types.InlineKeyboardButton("ASSINAR VIP R$9,99 (PIX)", callback_data="pagar_vip")
 
-        # Criar layout com o botão de pagamento
-        layout_pagamento = types.InlineKeyboardMarkup()
-        layout_pagamento.add(botao_pagamento)
+# Criar layout com o botão de pagamento
+layout_pagamento = types.InlineKeyboardMarkup()
+layout_pagamento.add(botao_pagamento)
 
-        # Enviar a mensagem com a imagem e o botão de pagamento
-        with open('./imagem/vip.png', 'rb') as photo:
-            bot.send_photo(call.message.chat.id, photo, caption="*♦️VIDEOS TODOS OS DIAS♦️*\n\n🔥+DE 99VIDEOS\n🔥SEXO\n🔥MASTURBAÇÃO\n🔥ANAL\n🔥SEXO A 3\n\nEstou esperando você bem molhadinha no meu VIP😍\n\n💠CLIQUE PRA VIRAR *VIP*💠", parse_mode="Markdown", reply_markup=layout_pagamento)
-
+# Enviar a mensagem com a imagem, o código do PIX e o botão de pagamento
+with open('./imagem/vip.png', 'rb') as photo:
+    message = bot.send_photo(call.message.chat.id, photo, caption=f"*♦️VIDEOS TODOS OS DIAS♦️*\n\n🔥+DE 99VIDEOS\n🔥SEXO\n🔥MASTURBAÇÃO\n🔥ANAL\n🔥SEXO A 3\n\nEstou esperando você bem molhadinha no meu VIP😍\n\n💠CLIQUE PARA PAGAR VIA PIX💠\n\n{pix_code}", parse_mode="Markdown", reply_markup=layout_pagamento)
 @bot.message_handler(func=lambda message: True)
 def mensagem_genérica(mensagem):
     # Criando o botão
